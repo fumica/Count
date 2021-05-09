c9e2801ca9cef3ba778c224866c1af4b979f145c
kawanofumica <1001Kfmk@gmail.com>
2021-05-09 17:13:40 +0900

    完成

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,11 +9,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
+    var clear: Int = 0
+    @IBOutlet var berry: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func plus () {
+        number = number + 1
+        label.text = String(number)
+        
+        if number >= 10 {
+            label.textColor = UIColor.red
+        }else if number <= -10 {
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.black
+        }
+    }
+    @IBAction func minus () {
+        number = number - 1
+        label.text = String(number)
+        if number >= 10 {
+            label.textColor = UIColor.red
+        }else if number <= -10 {
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.black
+        }
+    }
+    @IBAction func dial () {
+        number = number * 2
+        label.text = String(number)
+        if number >= 10 {
+            label.textColor = UIColor.red
+        }else if number <= -10 {
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.black
+        }
+    }
+    @IBAction func divide () {
+        number = number / 2
+        label.text = String(number)
+        if number >= 10 {
+            label.textColor = UIColor.red
+        }else if number <= -10 {
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.black
+        }
+    }
+    @IBAction func delete () {
+        number = 0
+        label.text = String(clear)
+        
+        if number >= 10 {
+            label.textColor = UIColor.red
+        }else if number <= -10 {
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.black
+        }
 
 }
-
+}
